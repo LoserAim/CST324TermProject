e72f900e2297c01feae21305ec395bf1a201def0
Andrew Doser <andrew.doser@oit.edu>
2018-11-28 14:33:33 -0800

    Added forms

---
+++ Application/CST324_TermProject_RideShare/Forms/form_Driver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CST324_TermProject_RideShare
+{
+    public partial class form_Driver : Form
+    {
+        public form_Driver()
+        {
+            InitializeComponent();
+        }
+    }
+}
