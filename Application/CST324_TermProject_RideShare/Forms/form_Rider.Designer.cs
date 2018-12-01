06a3aa1b5451c3030f7b7011de0750142e3ad818
Andrew Doser <andrew.doser@oit.edu>
2018-11-30 23:17:43 -0800

    Added simple validation

--- Application/CST324_TermProject_RideShare/Forms/form_Rider.Designer.cs
+++ Application/CST324_TermProject_RideShare/Forms/form_Rider.Designer.cs
@@ -34,14 +34,18 @@
             this.cb_State = new System.Windows.Forms.ComboBox();
             this.lb_streetaddress = new System.Windows.Forms.Label();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txt_StreetAddress = new System.Windows.Forms.TextBox();
             this.lb_UserPrompt_Location = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.lb_destination = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lb_destination = new System.Windows.Forms.Label();
+            this.txt_destination = new System.Windows.Forms.TextBox();
+            this.txt_DesiredTime = new System.Windows.Forms.TextBox();
             this.lb_DesiredTime = new System.Windows.Forms.Label();
             this.btn_SubmitRideRequest = new System.Windows.Forms.Button();
+            this.Rider_ErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.txt_CreditCard = new System.Windows.Forms.TextBox();
+            this.lb_CreditCard = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Rider_ErrorProvider)).BeginInit();
             this.SuspendLayout();
             // 
             // lb_title
@@ -90,12 +94,13 @@
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
-            // textBox1
+            // txt_StreetAddress
             // 
-            this.textBox1.Location = new System.Drawing.Point(350, 147);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(207, 20);
-            this.textBox1.TabIndex = 5;
+            this.txt_StreetAddress.Location = new System.Drawing.Point(350, 147);
+            this.txt_StreetAddress.Name = "txt_StreetAddress";
+            this.txt_StreetAddress.Size = new System.Drawing.Size(207, 20);
+            this.txt_StreetAddress.TabIndex = 5;
+            this.txt_StreetAddress.Validating += new System.ComponentModel.CancelEventHandler(this.txt_StreetAddress_Validating);
             // 
             // lb_UserPrompt_Location
             // 
@@ -117,28 +122,30 @@
             this.label1.TabIndex = 7;
             this.label1.Text = "Please enter your Desired Arrival time and Destination";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(271, 227);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(138, 13);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "Destination (Street Address)";
-            // 
             // lb_destination
             // 
-            this.lb_destination.Location = new System.Drawing.Point(430, 224);
+            this.lb_destination.AutoSize = true;
+            this.lb_destination.Location = new System.Drawing.Point(271, 227);
             this.lb_destination.Name = "lb_destination";
-            this.lb_destination.Size = new System.Drawing.Size(207, 20);
-            this.lb_destination.TabIndex = 9;
+            this.lb_destination.Size = new System.Drawing.Size(138, 13);
+            this.lb_destination.TabIndex = 8;
+            this.lb_destination.Text = "Destination (Street Address)";
             // 
-            // textBox2
+            // txt_destination
             // 
-            this.textBox2.Location = new System.Drawing.Point(430, 250);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(207, 20);
-            this.textBox2.TabIndex = 11;
+            this.txt_destination.Location = new System.Drawing.Point(430, 224);
+            this.txt_destination.Name = "txt_destination";
+            this.txt_destination.Size = new System.Drawing.Size(207, 20);
+            this.txt_destination.TabIndex = 9;
+            this.txt_destination.Validating += new System.ComponentModel.CancelEventHandler(this.txt_destination_Validating);
+            // 
+            // txt_DesiredTime
+            // 
+            this.txt_DesiredTime.Location = new System.Drawing.Point(430, 250);
+            this.txt_DesiredTime.Name = "txt_DesiredTime";
+            this.txt_DesiredTime.Size = new System.Drawing.Size(207, 20);
+            this.txt_DesiredTime.TabIndex = 11;
+            this.txt_DesiredTime.Validating += new System.ComponentModel.CancelEventHandler(this.txt_DesiredTime_Validating);
             // 
             // lb_DesiredTime
             // 
@@ -151,32 +158,59 @@
             // 
             // btn_SubmitRideRequest
             // 
-            this.btn_SubmitRideRequest.Location = new System.Drawing.Point(272, 294);
+            this.btn_SubmitRideRequest.Location = new System.Drawing.Point(272, 332);
             this.btn_SubmitRideRequest.Name = "btn_SubmitRideRequest";
             this.btn_SubmitRideRequest.Size = new System.Drawing.Size(285, 37);
             this.btn_SubmitRideRequest.TabIndex = 12;
             this.btn_SubmitRideRequest.Text = "Submit Ride Request";
             this.btn_SubmitRideRequest.UseVisualStyleBackColor = true;
+            this.btn_SubmitRideRequest.Click += new System.EventHandler(this.btn_SubmitRideRequest_Click);
+            // 
+            // Rider_ErrorProvider
+            // 
+            this.Rider_ErrorProvider.ContainerControl = this;
+            // 
+            // txt_CreditCard
+            // 
+            this.txt_CreditCard.Location = new System.Drawing.Point(430, 276);
+            this.txt_CreditCard.Name = "txt_CreditCard";
+            this.txt_CreditCard.Size = new System.Drawing.Size(207, 20);
+            this.txt_CreditCard.TabIndex = 14;
+            this.txt_CreditCard.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txt_CreditCard.Validating += new System.ComponentModel.CancelEventHandler(this.txt_CreditCard_Validating);
+            // 
+            // lb_CreditCard
+            // 
+            this.lb_CreditCard.AutoSize = true;
+            this.lb_CreditCard.Location = new System.Drawing.Point(271, 279);
+            this.lb_CreditCard.Name = "lb_CreditCard";
+            this.lb_CreditCard.Size = new System.Drawing.Size(99, 13);
+            this.lb_CreditCard.TabIndex = 13;
+            this.lb_CreditCard.Text = "Credit Card Number";
+            this.lb_CreditCard.Click += new System.EventHandler(this.label2_Click);
             // 
             // form_Rider
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_CreditCard);
+            this.Controls.Add(this.lb_CreditCard);
             this.Controls.Add(this.btn_SubmitRideRequest);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txt_DesiredTime);
             this.Controls.Add(this.lb_DesiredTime);
+            this.Controls.Add(this.txt_destination);
             this.Controls.Add(this.lb_destination);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lb_UserPrompt_Location);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txt_StreetAddress);
             this.Controls.Add(this.lb_streetaddress);
             this.Controls.Add(this.cb_State);
             this.Controls.Add(this.lb_City);
             this.Controls.Add(this.lb_title);
             this.Name = "form_Rider";
             this.Text = "form_Rider";
+            ((System.ComponentModel.ISupportInitialize)(this.Rider_ErrorProvider)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -189,13 +223,16 @@
         private System.Windows.Forms.ComboBox cb_State;
         private System.Windows.Forms.Label lb_streetaddress;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txt_StreetAddress;
         private System.Windows.Forms.Label lb_UserPrompt_Location;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox lb_destination;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lb_destination;
+        private System.Windows.Forms.TextBox txt_destination;
+        private System.Windows.Forms.TextBox txt_DesiredTime;
         private System.Windows.Forms.Label lb_DesiredTime;
         private System.Windows.Forms.Button btn_SubmitRideRequest;
+        private System.Windows.Forms.ErrorProvider Rider_ErrorProvider;
+        private System.Windows.Forms.TextBox txt_CreditCard;
+        private System.Windows.Forms.Label lb_CreditCard;
     }
 }
